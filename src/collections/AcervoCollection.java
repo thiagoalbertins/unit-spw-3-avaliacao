5d597858eabc6a82eed276be9e325f6979275f79
thiagoalbertins <thiagoalbertins@gmail.com>
2019-10-26 11:55:05 -0300

    Update AcervoCollection.java

--- src/collections/AcervoCollection.java
+++ src/collections/AcervoCollection.java
@@ -7,6 +7,14 @@ public class AcervoCollection {
 	
 	ArrayList<Acervo> acervo = new ArrayList<>();
 	
+	public void adicionarAcervo(Acervo a) {
+		acervo.add(a);
+	}
+	
+	public void removerAcervo() {
+		
+	}
+	
 	
 	//Criar CRUD para acervo
 	
