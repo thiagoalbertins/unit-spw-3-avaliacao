b8e5701506952e5cc6ea3f173bc24af3abaa1029
alvesmog <alvesmog@gmail.com>
2019-10-26 11:00:45 -0300

    Colletions "Acervos" e "Pessoas" criadas

---
+++ src/collections/AcervoCollection.java
@@ -0,0 +1,12 @@
+package collections;
+import java.util.ArrayList;
+import gestaoDeAcervo.Acervo;
+
+
+public class AcervoCollection {
+	
+	ArrayList<Acervo> acervos = new ArrayList<>();
+	
+	//Criar CRUD para acervo
+
+}
---
+++ src/collections/PessoaCollection.java
@@ -0,0 +1,13 @@
+package collections;
+
+import java.util.ArrayList;
+
+import gestaoDePessoa.Pessoa;
+
+public class PessoaCollection {
+	
+	ArrayList<Pessoa> pessoas = new ArrayList<>();
+	
+	//Criar CRUD para a pessoa
+
+}
