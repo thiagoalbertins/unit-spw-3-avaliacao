ed4aa187d1856b9f977c35313b01c893735d8974
thiagoalbertins <thiagoalbertins@gmail.com>
2019-10-26 10:16:55 -0300

    Update Acervo.java

--- src/gestaoAcervo/Acervo.java
+++ src/gestaoAcervo/Acervo.java
@@ -2,4 +2,33 @@ package gestaoAcervo;
 
 public class Acervo {
 
+	int codigo, anoPublicacao, numeroPaginas;
+	String volume;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public int getAnoPublicacao() {
+		return anoPublicacao;
+	}
+	public void setAnoPublicacao(int anoPublicacao) {
+		this.anoPublicacao = anoPublicacao;
+	}
+	public int getNumeroPaginas() {
+		return numeroPaginas;
+	}
+	public void setNumeroPaginas(int numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+	public String getVolume() {
+		return volume;
+	}
+	public void setVolume(String volume) {
+		this.volume = volume;
+	}
+	
+	
 }
