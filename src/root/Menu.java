2f6af7a985bfe1fb4f35e1b9c3892cfb7915aa89
alvesmog <alvesmog@gmail.com>
2019-10-26 14:46:30 -0300

    Início da implementação dos menus

--- src/root/Menu.java
+++ src/root/Menu.java
@@ -2,14 +2,53 @@ package root;
 
 public class Menu {
 	
-	public static void menuInicial() {
+	//1-Menu inicial
+	//2-Acervo
+	//3-Pessoas
+	
+	int id;
+	
+	public static void renderizar(String i) {
 		
-		System.out.println("Sistema Biblioteca"+
-						"\n 1-Acervo"+
-						"\n 2-Pessoas"
-				);
+		switch(i) {
 		
+		case "1": menuInicial();
+				break;
+				
+		case "2": menuAcervo();
+				break;
+		
+		case "3": menuPessoas();
+				break;
+				
+		}
 		
 	}
+	
+	
+	public static void menuInicial() {
+		
+		System.out.println("Sistema Biblioteca \n"+
+						"\n 1 - Acervo"+
+						"\n 2 - Pessoas"
+				);
+	}
+	
+	public static void menuAcervo() {
+		
+		System.out.println("Menu Acervo \n"+
+						"\n [v] - Voltar"
+				);
+		
+	}
+	
+	public static void menuPessoas() {
+		
+		System.out.println("Menu Pessoas \n"+
+						"\n [v] - Voltar"
+				);
+		
+	}
+	
 
 }
