e206f71d5ad894d1fbe240c8c78eb38329accf9a
thiagoalbertins <thiagoalbertins@gmail.com>
2019-11-13 12:02:16 -0300

    Update Emprestimo.java

--- src/gestaoDeEmprestimo/Emprestimo.java
+++ src/gestaoDeEmprestimo/Emprestimo.java
@@ -1,5 +1,25 @@
 package gestaoDeEmprestimo;
 
-public class Emprestimo {
+import java.util.Date;
+import gestaoDePessoa.Usuario;
+import gestaoDeAcervo.Acervo;
 
+public class Emprestimo {
+	
+	private Usuario usuario;
+	private Acervo acervo;
+	private String status;
+	private Date data;
+	
+	public Emprestimo(Usuario usuario, Acervo acervo, String status, Date data) {
+		super();
+		this.usuario = usuario;
+		this.acervo = acervo;
+		this.status = status;
+		this.data = data;
+	}
+
+	public void registrarEmprestimo() {
+		
+	}
 }
