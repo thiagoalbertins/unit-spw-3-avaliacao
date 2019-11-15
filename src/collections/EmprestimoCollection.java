6b14b1f2fb5a0ebf76330cc8290f14fab6ee16fc
alvesmog <41652807+alvesmog@users.noreply.github.com>
2019-11-15 16:00:30 -0300

    Empréstimos concluído

--- src/collections/EmprestimoCollection.java
+++ src/collections/EmprestimoCollection.java
@@ -15,7 +15,7 @@ public class EmprestimoCollection {
 
 	public static void listarEmprestimo() {
 		for (int i = 0; i < emprestimos.size(); i++) {
-			System.out.println(
+			System.out.println(i+
 					  " ||Usuário:" + emprestimos.get(i).getUsuario().getNome() 
 					+ " ||Código do acervo: " + emprestimos.get(i).getAcervo().getCodigo() 
 					+ " ||Data de emprestimos: " + emprestimos.get(i).getData() 
@@ -23,12 +23,15 @@ public class EmprestimoCollection {
 		}
 	}
 
-	public static void devolução(Emprestimo a) {
-		emprestimos.remove(a);
+	public static void devolução(Emprestimo e) {
+		emprestimos.remove(e);
 	}
 	
 	public static int tamanhoLista() {
 		return emprestimos.size();
 	}
-
+	
+	public static Emprestimo buscarEmprestimo(int i) {	
+		return emprestimos.get(i);
+	}
 }
